c1cf45889438057db97553f4af4976d9e3cf1118
Chau-Truong-Phat <chautruongphat123@gmail.com>
2020-06-27 22:44:10 +0700

    update Phat task

--- DHKTPM13A_TruongPhat_VietTuan_CongLoc/QuickSort/UnitTestQuickSort.cs
+++ DHKTPM13A_TruongPhat_VietTuan_CongLoc/QuickSort/UnitTestQuickSort.cs
@@ -12,10 +12,32 @@ namespace QuickSort
             MethodLibrary.MethodLibrary obj = new MethodLibrary.MethodLibrary();
 
             int []a = {1,3,2,4,5};
-            int []Expected = {1,2,3,4,5};
+            int []expectedResult = {1,2,3,4,5};
             obj.QuickSort(a, 0, 4);
 
-            Assert.AreEqual(Expected, a);
+            Assert.AreEqual(expectedResult, a);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            MethodLibrary.MethodLibrary obj = new MethodLibrary.MethodLibrary();
+
+            int[] a = { 1, 3, 2, 4, 5 };
+            int[] expectedResult = { 1, 2, 3, 4, 5 };
+            obj.QuickSort(a, -1, 4);
+
+            Assert.AreEqual(expectedResult, a);
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            MethodLibrary.MethodLibrary obj = new MethodLibrary.MethodLibrary();
+
+            int[] a = { 1, 3, 2, 4, 5 };
+            int[] expectedResult = { 1, 2, 3, 4, 5 };
+            obj.QuickSort(a, 0, 5);
+
+            Assert.AreEqual(expectedResult, a);
         }
     }
 }
--- DHKTPM13A_TruongPhat_VietTuan_CongLoc/ThayThe/UnitTestThayTheDataDriven.cs
+++ DHKTPM13A_TruongPhat_VietTuan_CongLoc/ThayThe/UnitTestThayTheDataDriven.cs
@@ -6,24 +6,44 @@ namespace ThayThe
     [TestClass]
     public class UnitTestThayTheDataDriven
     {
-        public TestContext TestContext { get; set; }
-        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV",
-            "|DataDirectory|\\dataThayThe.csv", "dataThayThe#csv", DataAccessMethod.Sequential),
-                DeploymentItem("dataThayThe.csv"), TestMethod]
+        //public TestContext TestContext { get; set; }
 
-        public void TestMethod1()
+        //[TestMethod]
+        //[DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV",
+        //    "|DataDirectory|\\dataThayThe.csv", "dataThayThe#csv",
+        //    DataAccessMethod.Sequential), DeploymentItem("dataThayThe.csv")]
+        //public void TestMethod1()
+        //{
+        //    MethodLibrary.MethodLibrary obj = new MethodLibrary.MethodLibrary();
+        //    string a, b, c;
+        //    a = Convert.ToString(TestContext.DataRow[0]);
+        //    b = Convert.ToString(TestContext.DataRow[1]);
+        //    c = Convert.ToString(TestContext.DataRow[2]);
+
+        //    string Expected = Convert.ToString(TestContext.DataRow[3]);
+        //    string Actual = obj.ThayThe(a, b, c);
+
+        //    Assert.AreEqual(Expected, Actual);
+        //}
+
+        public TestContext TestContext { get; set; }
+        [TestMethod]
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV",
+            "|DataDirectory|\\dataThayThe.csv", "dataThayThe#csv",
+                DataAccessMethod.Sequential), DeploymentItem("dataThayThe.csv")]
+        public void TestDGN()
         {
             MethodLibrary.MethodLibrary obj = new MethodLibrary.MethodLibrary();
+
             string a, b, c;
-            a = Convert.ToString(TestContext.DataRow[0]);
-            b = Convert.ToString(TestContext.DataRow[1]);
-            c = Convert.ToString(TestContext.DataRow[2]);
+            a = TestContext.DataRow[0].ToString();
+            b = TestContext.DataRow[1].ToString();
+            c = TestContext.DataRow[2].ToString();
 
-            string Expected = Convert.ToString(TestContext.DataRow[3]);
-            string Actual = obj.ThayThe(a, b, c);
+            string expectedResult = TestContext.DataRow[3].ToString();
+            string actualResult = obj.ThayThe(a, b, c);
 
-            Assert.AreEqual(Expected, Actual);
+            Assert.AreEqual(expectedResult, actualResult);
         }
-
     }
 }
